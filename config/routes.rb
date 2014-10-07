934ca9fe05d0ca43e39d529b095b90a88e481eea
Julie Torres <reeddetorres@gmail.com>
2014-10-07 10:51:35 -0400

    Fix devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
   devise_for :users
+  get 'static_pages/home'
   root to: "static_pages#home"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
