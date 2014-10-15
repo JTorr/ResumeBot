93649b09f23f221c49e9a819aade18033eb4158c
Julie Torres <reeddetorres@gmail.com>
2014-10-15 19:07:07 -0400

    Minor view changes

--- app/models/resume.rb
+++ app/models/resume.rb
@@ -5,5 +5,5 @@ class Resume < ActiveRecord::Base
   has_many :resume_experiences
   has_many :experiences, through: :resume_experiences
   has_many :educations
-  validates_presence_of :email, :first_name, :last_name, :phone_1, :address, :postal_code, :city, :state
+  validates_presence_of :title, :email, :first_name, :last_name, :phone_1, :address, :postal_code, :city, :state
 end
