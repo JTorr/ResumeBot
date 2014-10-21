b5ec13758a0752bb4f3fb3cadc96aa860262bfad
Julie Torres <reeddetorres@gmail.com>
2014-10-21 10:21:56 -0400

    First crack at refactoring PDF

--- app/pdfs/resume_pdf.rb
+++ app/pdfs/resume_pdf.rb
@@ -7,17 +7,28 @@ class ResumePdf < Prawn::Document
     @resume = resume
     header
     profile
+    sections
+  end
 
+  def sections
+    #TODO: refactor to check each method, then run non-empty methods
     unless @resume.experiences.empty?
       employment
     end
-    # binding.pry
+
     unless @resume.skills.count == 0
       skills
     end
 
     unless @resume.educations.empty?
       education
+    end
+  end
+
+  def underline
+    stroke_color "ff0000"
+    stroke do
+      horizontal_rule
     end
   end
 
@@ -35,10 +46,7 @@ class ResumePdf < Prawn::Document
       text "#{@resume.address}", size: 10, align: :right
       text "#{@resume.city}" + ", " + "#{@resume.state}" + "#{@resume.postal_code}", size:10, align: :right
     end
-
-  stroke_color "ff0000"
-  stroke do
-     horizontal_rule
+    underline
   end
 
   def profile
@@ -50,12 +58,7 @@ class ResumePdf < Prawn::Document
     bounding_box([150, @y_position - 105], :width => 270, :height => 20) do
       text "Instant Robot Coder Ninja! Just add coffee.", size:12, align: :right
     end
-
-    stroke_color "ff0000"
-      stroke do
-         horizontal_rule
-      end
-    end
+    underline
   end
 
   def employment
@@ -67,22 +70,17 @@ class ResumePdf < Prawn::Document
 
     count = 0
     exp = @resume.experiences.each do |exp|
-        count += 1
-        @y1 = @y_position - 55 - (count * 90)
-        bounding_box([180, @y1], :width => 270, :height => 180) do
-          # binding.pry
-          text "#{exp.company_name}", size: 18, align: :left
-          text "#{exp.position}", size: 14, align: :left, style: :italic
-          text "#{exp.summary}", size: 14, align: :left
-        end
-        move_up 100
-    end
+      count += 1
+      @y1 = @y_position - 55 - (count * 90)
 
-    # stroke_color "ff0000"
-      stroke do
-         horizontal_rule
+      bounding_box([180, @y1], :width => 270, :height => 180) do
+        text "#{exp.company_name}", size: 18, align: :left
+        text "#{exp.position}", size: 14, align: :left, style: :italic
+        text "#{exp.summary}", size: 14, align: :left
       end
     end
+    move_up 100
+  end
 
   def skills
     move_down 10
@@ -94,44 +92,37 @@ class ResumePdf < Prawn::Document
     exp = @resume.skills.each do |skill|
       count += 1
       @y2 = @y1 - 55 - (count * 20)
+      #TODO: break out into bullet method
       float do
         bounding_box [170, @y2 - 30], :width => 10 do
           text "•"
         end
       end
       bounding_box([180, @y2 - 30], :width => 270, :height => 140) do
-        # binding.pry
         text "#{skill.name}", size: 14, align: :left
       end
+    end
       move_up 100
       @c = cursor
+      underline
+  end
+
+  def education
+    move_down 10
+    bounding_box([20, @c - 10], :width => 270, :height => 180) do
+      text "Education", size:18, align: :left
     end
 
-    # stroke_color "ff0000"
-      stroke do
-         horizontal_rule
+    count = 0
+    ed = @resume.educations.each do |ed|
+      count += 1
+      @y3 = @c + 70 - (count * 90)
+      bounding_box([180, @y3], :width => 270, :height => 180) do
+        text "#{ed.institution}", size: 18, align: :left
+        text "#{ed.area}", size: 14, align: :left, style: :italic
+        text "#{ed.study_type}", size: 14, align: :left
       end
+      move_up 100
     end
-
-    def education
-      move_down 10
-      bounding_box([20, @c - 10], :width => 270, :height => 180) do
-        text "Education", size:18, align: :left
-      end
-
-      count = 0
-      ed = @resume.educations.each do |ed|
-        count += 1
-        @y3 = @c + 70 - (count * 90)
-        bounding_box([180, @y3], :width => 270, :height => 180) do
-          text "#{ed.institution}", size: 18, align: :left
-          text "#{ed.area}", size: 14, align: :left, style: :italic
-          text "#{ed.study_type}", size: 14, align: :left
-        end
-        move_up 100
-      end
-
-      end
-
-
+  end
 end
